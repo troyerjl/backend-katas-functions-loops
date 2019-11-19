a3a80e96be1a1b7388fec4a10c45bcc1dafd0fd7
Jonas Troyer <troyerjl2011@gmail.com>
2019-11-19 11:35:12 -0800

    Wrote five katas with only arithmatic.

--- main.py
+++ main.py
@@ -1,37 +1,58 @@
 #!/usr/bin/env python
-"""Implements math functions without using operators except for '+' """
 
-__author__ = "???"
+__author__ = "troyerjl"
 
 
 def add(x, y):
-    """Add two integers. Handles negative values."""
-    # your code here
-    return
+    product = x + y
+    return product
+
+
+def helper_multiply(x, y):
+    product = 0
+    for i in range(0, abs(y)):
+        product += x
+    return product
 
 
 def multiply(x, y):
-    """Multiply x with y. Handles negative values of x or y."""
-    # your code here
-    return
+    product = helper_multiply(x, y)
+    if x >= 0 and y >= 0:
+        return product
+    elif x < 0 and y < 0:
+        return abs(product)
+    elif x < 0 or y > 0:
+        return product
+    elif x > 0 and y < 0:
+        return -product
 
 
 def power(x, n):
-    """Raise x to power n, where n >= 0"""
-    # your code here
-    return
+    product = 1
+    for i in range(0, n):
+        product = multiply(product, x)
+    return product
 
 
 def factorial(x):
-    """Compute factorial of x, where x > 0"""
-    # your code here
-    return
+    product = 1
+    for i in range(1, x+1):
+        product = multiply(product, i)
+    return product
 
 
 def fibonacci(n):
-    """Compute the nth term of fibonacci sequence"""
-    # your code here
-    return
+    a, b = 1, 1
+    if n == 0:
+        return b
+    elif n == 1:
+        return b
+    else:
+        for i in range(2, n):
+            c = add(a, b)
+            a = b 
+            b = c 
+        return b 
 
 
 if __name__ == '__main__':
